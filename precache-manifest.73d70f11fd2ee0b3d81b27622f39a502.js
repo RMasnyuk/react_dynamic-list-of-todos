96df7039cd84b7c9c8250bb00a064adeca71ad28
Rostislav Masniuk <rostislav.masnyuk.98@gmail.com>
2019-06-13 19:04:25 +0300

    Updates

---
+++ precache-manifest.73d70f11fd2ee0b3d81b27622f39a502.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d330842d279ea4cb30878ccdcb94bf",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "62456f6e5851f8f970cd",
+    "url": "/react_dynamic-list-of-todos/static/css/main.140c7bae.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/react_dynamic-list-of-todos/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "62456f6e5851f8f970cd",
+    "url": "/react_dynamic-list-of-todos/static/js/main.2a542795.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
